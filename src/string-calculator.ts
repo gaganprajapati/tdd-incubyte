4e0c9fa6510f9d724877789c6fa8ba6c0586ee89
Gagan Prajapati <gagan.prajapati@scalenut.com>
2025-04-16 01:31:11 +0530

    Added more tests to get delimiter

--- src/string-calculator.ts
+++ src/string-calculator.ts
@@ -1,15 +1,18 @@
-const getDelimiter = (numbersString: string) => {
-    return ''
-}
+const getDelimiter = (numbersString: string): string => {
+  const match = numbersString.match(/\/\/(.*?)\n/);
+  return match?.length ? match[1] : '';
+};
 
 export const add = (numbersString: string): number => {
-    let delimiterRegEx = /[,\n]/;
-    if(numbersString.startsWith('//')) {
-        delimiterRegEx = new RegExp(getDelimiter(numbersString)); 
-    }
-    return numbersString.split(delimiterRegEx).reduce((acc, num) => acc + Number(num), 0)
-}
+  let delimiterRegEx = /[,\n]/;
+  if (numbersString.startsWith('//')) {
+    delimiterRegEx = new RegExp(getDelimiter(numbersString));
+  }
+  return numbersString
+    .split(delimiterRegEx)
+    .reduce((acc, num) => acc + Number(num), 0);
+};
 
 export const exportedForTesting = {
-    getDelimiter
-}
+  getDelimiter,
+};
--- tests/string-calculator.test.ts
+++ tests/string-calculator.test.ts
@@ -1,30 +1,36 @@
-import { add, exportedForTesting } from "../src/string-calculator"
+import { add, exportedForTesting } from '../src/string-calculator';
 
 describe('Add numbers', () => {
-    it('should return 0 for empty string', () => {
-        expect(add('')).toBe(0)
-    });
+  it('should return 0 for empty string', () => {
+    expect(add('')).toBe(0);
+  });
 
-    it('should return the number if string has only one number', () => {
-        expect(add('2')).toBe(2);
-        expect(add('12')).toBe(12);
-    });
+  it('should return the number if string has only one number', () => {
+    expect(add('2')).toBe(2);
+    expect(add('12')).toBe(12);
+  });
 
-    it('should return the sum of numbers separated by (,) in the string', () => {
-        expect(add('2,5')).toBe(7);
-        expect(add('12,20,10')).toBe(42);
-        expect(add('2,8,3,7,4,6,5,5')).toBe(40);
-    });
+  it('should return the sum of numbers separated by (,) in the string', () => {
+    expect(add('2,5')).toBe(7);
+    expect(add('12,20,10')).toBe(42);
+    expect(add('2,8,3,7,4,6,5,5')).toBe(40);
+  });
 
-    it('should return the sum of numbers separated by (,) or (\n) in the string', () => {
-        expect(add('2\n5')).toBe(7);
-        expect(add('12\n20,10')).toBe(42);
-        expect(add('2,8,3\n7,4,6\n5,5')).toBe(40);
-    });
-})
+  it('should return the sum of numbers separated by (,) or (\n) in the string', () => {
+    expect(add('2\n5')).toBe(7);
+    expect(add('12\n20,10')).toBe(42);
+    expect(add('2,8,3\n7,4,6\n5,5')).toBe(40);
+  });
+});
 
 describe('getDelimiter', () => {
-    it('should return empty string for empty string input', () => {
-        expect(exportedForTesting.getDelimiter('')).toBe('')
-    })
-})
+  it('should return empty string for empty string input', () => {
+    expect(exportedForTesting.getDelimiter('')).toBe('');
+  });
+
+  it('should return the character that is enclosed within "//" and "\n"', () => {
+    expect(exportedForTesting.getDelimiter('//;\n1;2')).toBe(';');
+    expect(exportedForTesting.getDelimiter('//*:\n1*:2')).toBe('*:');
+  });
+  
+});
