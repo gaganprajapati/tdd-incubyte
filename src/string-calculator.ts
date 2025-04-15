7bcc1767698d02f2687488c3684412a6cd7bb864
Gagan Prajapati <gagan.prajapati@scalenut.com>
2025-04-16 01:17:21 +0530

    Accepting \n as well as a separator for getting the sum

--- src/string-calculator.ts
+++ src/string-calculator.ts
@@ -1,3 +1,3 @@
 export const add = (numbersString: string): number => {
-    return numbersString.split(',').reduce((acc, num) => acc + Number(num), 0)
+    return numbersString.split(/[,\n]/).reduce((acc, num) => acc + Number(num), 0)
 }
--- tests/string-calculator.test.ts
+++ tests/string-calculator.test.ts
@@ -11,8 +11,14 @@ describe('Add numbers', () => {
     });
 
     it('should return the sum of numbers separated by (,) in the string', () => {
-        expect(add('2, 5')).toBe(7);
-        expect(add('12, 20, 10')).toBe(42);
-        expect(add('2, 8, 3, 7, 4, 6, 5, 5')).toBe(40);
+        expect(add('2,5')).toBe(7);
+        expect(add('12,20,10')).toBe(42);
+        expect(add('2,8,3,7,4,6,5,5')).toBe(40);
+    });
+
+    it('should return the sum of numbers separated by (,) or (\n) in the string', () => {
+        expect(add('2\n5')).toBe(7);
+        expect(add('12\n20,10')).toBe(42);
+        expect(add('2,8,3\n7,4,6\n5,5')).toBe(40);
     });
 })
